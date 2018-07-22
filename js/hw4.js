2d30b6d6ab7c82e74880e15dbf24f0561dc2ef77
brockcarter <brockcarter@gmail.com>
2018-07-22 14:05:09 -0400

    problem  3 solved - select fields

--- js/hw4.js
+++ js/hw4.js
@@ -6,7 +6,7 @@ window.onload = function () {
     var pwd1 = document.getElementById("pwd1");
     var pwd2 = document.getElementById("pwd2");
 
-    //Users must enter passwords of at least 8 characters
+    //users must enter passwords of at least 8 characters
     pwd1.oninput = function() {
         if (pwd1.value.length < 8) {
             document.getElementById("pwd1Hint").style.display = "inline";
@@ -14,7 +14,7 @@ window.onload = function () {
             document.getElementById("pwd1Hint").style.display = "none";
         }
     }
-    //The two fields must match
+    //the two fields must match
     pwd2.oninput = function () {
         if (pwd1.value != pwd2.value) {
             document.getElementById("pwd2Hint").style.display = "inline";
@@ -23,11 +23,60 @@ window.onload = function () {
         }
     }
     
+    
     // 2.) BIO WORD COUNT
     var bioBox = document.getElementById("bio");
     var charsLeft = document.getElementById("charsLeft");
-
+    //character limit minus the characters in bioBox, then placed in innerHTML
     bioBox.oninput = function() {
         charsLeft.innerHTML = 140 - bioBox.value.length;
     }
+
+
+
+    // 3.) SELECT FIELDS
+    //grab the two selectors
+    var firstSelect = document.getElementById("firstSelect");
+    var secondSelect = document.getElementById("secondSelect");
+
+    //create arrays that will populate selectors as options
+    var make = ["Land Rover", "Toyota"];
+    var roverModels = ["Discovery", "Range Rover", "Evoque"];
+    var toyotaModels = ["Land Cruiser", "4Runner", "FJ Cruiser"];
+    
+    //takes in an array and selector to populate options
+    function optionMaker(array, selectList) {
+        for (var i = 0; i < array.length; i++) {
+        //creates an option element, ie <option></option>
+        var option = document.createElement("option");
+        //fills that option with an array item
+        option.value = array[i];
+        option.text = array[i];
+        //appends that option to the input selector
+        selectList.appendChild(option);
+        }
+    } 
+   
+    //run so we have options in the first selector immediately on load
+    optionMaker(make, firstSelect);
+
+    //when first selector changes, first clean out residual options
+    firstSelect.onchange = function(){
+        while (secondSelect.childElementCount >= 2) {   
+            secondSelect.removeChild(secondSelect.lastChild);
+        }
+        //then run this to repopulate secondSelector based on what's in firstSelector
+        //have to call function, and not declare in here or it creates infinite loop
+        secondSelectorFiller();
+    }
+
+    //populate secondSelector based on what's in firstSelector
+    function secondSelectorFiller() {
+        if (firstSelect.value == "Land Rover") {
+            optionMaker(roverModels, secondSelect);
+        } else {
+            optionMaker(toyotaModels, secondSelect);
+        }
+    }
+
 }
