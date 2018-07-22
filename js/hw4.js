29cb7823caaf14619c49db32ebd04d53c14e43d1
Brock Carter <brockcarter@Brocks-MacBook-Pro.local>
2018-07-22 10:49:35 -0400

    solved first problem - password check

--- js/hw4.js
+++ js/hw4.js
@@ -1,1 +1,25 @@
 /* hw4.js */
+
+// 1.) PASSWORD CHECK
+window.onload = function () {
+    "use strict";
+    var pwd1 = document.getElementById("pwd1");
+    var pwd2 = document.getElementById("pwd2");
+
+    //Users must enter passwords of at least 8 characters
+    pwd1.oninput = function() {
+        if (pwd1.value.length < 8) {
+            document.getElementById("pwd1Hint").style.display = "inline";
+        } else{
+            document.getElementById("pwd1Hint").style.display = "none";
+        }
+    }
+    //The two fields must match
+    pwd2.oninput = function () {
+        if (pwd1.value != pwd2.value) {
+            document.getElementById("pwd2Hint").style.display = "inline";
+        } else {
+            document.getElementById("pwd2Hint").style.display = "none";
+        }
+    }  
+}
